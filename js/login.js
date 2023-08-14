73d2daec2a57343ec15fc7cfb7e8280b1147ef50
Karen <giulianaferreira2008@gmail.com>
2023-08-14 20:28:45 -0300

    Solucion de errores y creacion de archivo redireccion.js

--- js/login.js
+++ js/login.js
@@ -1,17 +1,3 @@
-// obtenemos el valor en Local Storage
-const sesionIniciada = localStorage.getItem("sesionIniciada");
-
-// verificar el valor
-if (!sesionIniciada) {
-    //si no lo encuentra lo creea y lo guarda
-  if (!JSON.parse(localStorage.getItem("sesionIniciada"))) {
-    // guardo el valor de la sesion en el Local Storage convirtiendolo en texto antes
-    localStorage.setItem("sesionIniciada", JSON.stringify(false));
-  }
-  // Redireccionar a la página de inicio de sesión
-  window.location.href = "login.html";
-
-}
 
 
 // capturamos el boton y agregamos el escuchador de eventos cambiando el valor a true y redireccionando
@@ -22,3 +8,5 @@ document
     localStorage.setItem("sesionIniciada", "true");
     window.location.href = "index.html";
   });
+
+
