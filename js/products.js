4988f2a9be33ddb5521b8c96412e8ff155a6af4c
guzmansantanacola <guzmansantanacola@gmail.com>
2023-08-17 00:21:31 -0300

    categorias

--- js/products.js
+++ js/products.js
@@ -11,20 +11,20 @@ fetch ("https://japceibal.github.io/emercado-api/cats_products/101.json")
         autos.forEach(products => {
                    htmlContentToAppend += `
                    <div setCatID(${products.id})" class="fondolista">
-                       <div class="row">
-                           <div class="col-3">
+                       <div class="fila">
+                           <div class=imagenes>
                                <img src="${products.image}" alt="${products.description}" >
                            </div>
-                           <div class="col">
-                               <div class="d-flex w-100 justify-content-between">
+                           <div class="item">
+                               <div>
                                    <h4 class="nombreproductos">${products.name} </h4>
-                                   <small class="text-muted">${products.soldCount} vendidos</small>
+                                   <p class="precio">${products.currency} ${products.cost}</p>
                                </div>
-                               <p class="description">${products.description}</p>
+                               <p>${products.description}</p>
                                
                            </div>
                        </div>
-                       <p class="precio">${products.currency} ${products.cost}</p>
+                       <h5 class="vendidos">${products.soldCount} vendidos</h5>
                    </div>
                    `
                document.getElementById("productos").innerHTML = htmlContentToAppend;
