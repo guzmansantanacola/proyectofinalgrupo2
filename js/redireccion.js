379dd68bc00967012785eb49ad39d2affd233578
guzmansantanacola <guzmansantanacola@gmail.com>
2023-08-18 16:01:38 -0300

    arreglo productos

--- js/redireccion.js
+++ js/redireccion.js
@@ -4,7 +4,7 @@ const sesionIniciada = JSON.parse(localStorage.getItem("sesionIniciada"));
 
 // verificar el valor
 if (!sesionIniciada) {
-    //si no lo encuentra lo creea y lo guarda
+    //si no lo encuentra lo cree y lo guarda
   
   // Redireccionar a la página de inicio de sesión
   window.location.href = "login.html";
