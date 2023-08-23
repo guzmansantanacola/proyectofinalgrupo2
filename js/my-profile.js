908f5bcf8611eddd81125111e3f65331bbdea3b0
Karen <giulianaferreira2008@gmail.com>
2023-08-23 12:46:00 -0300

    agrego id al nombre de usuario

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,8 +1,12 @@
-/* Obtengo el nombre de usuario con en el login.html*/
-const inputElement=document.querySelector(input-bx);
 
-/*Creo otra constante para llamar al nombre*/
-const nombre=inputElement.value;
-/* Guardo en el localStorage*/
-localStorage.setItem(usuario, nombre);
-/* crear un div en el index.html */
+function guardarUsuario(){ 
+/* Obtengo el nombre de usuario con en el login.html*/
+const inputElement=document.getElementById("nombredeusuario").value;
+alert("usuario:" + inputElement);
+// /*Creo otra constante para llamar al nombre*/
+// const nombre=inputElement.value;
+// /* Guardo en el localStorage*/
+// localStorage.setItem(usuario, nombre);
+// /* crear un div en el index.html */
+
+}
