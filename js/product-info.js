588ad262b8cbca3d1b7abf9845f5019c8780692e
Karen <giulianaferreira2008@gmail.com>
2023-08-28 21:06:37 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -2,7 +2,12 @@
 
 const product=document.getElementById("searchInput");
 searchInput.addEventListener("input", () =>{
-    
-}
 
-)
+    const searchFinal= searchInput.value;
+    const filtro= product.filter(product =>
+        const titulo=product.name;
+        const descrip= product.description;
+    
+    });
+    
+    /**incluyo el titulo y descripcion */
