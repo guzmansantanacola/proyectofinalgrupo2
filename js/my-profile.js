5a76b5e43d771a392157310d1a7927dbb2d618cf
Karen <giulianaferreira2008@gmail.com>
2023-09-23 18:39:40 -0300

    desplegable

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,31 @@
 
-usuarioguardado = localStorage.getItem("nombredeusuario");
-document.getElementById("nombreusuario").innerHTML = usuarioguardado;
+
+    usuarioguardado = localStorage.getItem("nombredeusuario");
+    document.getElementById("nombreusuario").innerHTML = usuarioguardado;
+    
+/** DESPLEGABLE */
+
+
+    document.addEventListener("DOMContentLoaded", function () {
+let nombreUsuarioButton = document.getElementById("nombredeusuario");
+let menuDesplegable = document.createElement("ul");
+    menuDesplegable.id = "menu-desplegable";
+   
+    menuDesplegable.innerHTML = `
+        <li><a href="carrito.html">Mi Carrito</a></li>
+        <li><a href="my-profile.html">Mi Perfil</a></li>
+        <li><button id="cerrarSesion">Cerrar Sesión</button></li>
+    `;
+nombreUsuarioButton.addEventListener("click", function (){
+    if (menuDesplegable.style.display === "block") {
+        menuDesplegable.style.display = "none";
+    } else {
+        menuDesplegable.style.display = "block";
+    }
+});
+document.body.appendChild(menuDesplegable);
+});
+
+
+
+
