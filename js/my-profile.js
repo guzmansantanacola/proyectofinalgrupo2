ddf3a4dee75b6193e1f12b4f74a386fd54bc7cfc
Karen <giulianaferreira2008@gmail.com>
2023-09-23 20:07:08 -0300

    quedó, faltan estilos

--- js/my-profile.js
+++ js/my-profile.js
@@ -7,18 +7,29 @@
 
 
     document.addEventListener("DOMContentLoaded", function () {
-let nombreUsuarioButton = document.getElementById("nombredeusuario");
+let nombreUsuarioButton = document.getElementById("nombreusuario");
 let menuDesplegable = document.createElement("ul");
     menuDesplegable.id = "menu-desplegable";
    
     menuDesplegable.innerHTML = `
-        <li><a href="carrito.html">Mi Carrito</a></li>
-        <li><a href="my-profile.html">Mi Perfil</a></li>
-        <li><button id="cerrarSesion">Cerrar Sesión</button></li>
+        <li class="iguales"><a href="carrito.html">Mi Carrito</a></li>
+        <li class="iguales" ><a href="my-profile.html">Mi Perfil</a></li>
+        <li class="iguales"><button id="cerrarSesion">Cerrar Sesión</button></li>
     `;
+    menuDesplegable.style.display = "none";
 nombreUsuarioButton.addEventListener("click", function (){
-    if (menuDesplegable.style.display === "block") {
-        menuDesplegable.style.display = "none";
+
+    const rect = nombreUsuarioButton.getBoundingClientRect();
+    const top = rect.bottom;
+    const left = rect.left;
+
+    //
+    menuDesplegable.style.position = "fixed";
+    menuDesplegable.style.top = `${top}px`;
+    menuDesplegable.style.left = `${left}px`;
+  
+    if (menuDesplegable.style.display === "none") {
+        menuDesplegable.style.display = "block";
     } else {
         menuDesplegable.style.display = "block";
     }
