cc6c7bb21519172a8c3109d3d7dea19f54ad94fa
Karen <giulianaferreira2008@gmail.com>
2023-09-24 10:32:24 -0300

    cambios, css

--- js/my-profile.js
+++ js/my-profile.js
@@ -6,25 +6,28 @@
     
 /** DESPLEGABLE */
 
-
     document.addEventListener("DOMContentLoaded", function () {
-let nombreUsuarioButton = document.getElementById("nombreusuario");
-let menuDesplegable = document.createElement("ul");
+
+    let nombreUsuarioButton = document.getElementById("nombreusuario");
+
+    let menuDesplegable = document.createElement("ul");
+
     menuDesplegable.id = "menu-desplegable";
+
     let menu = localStorage.getItem("menuVisible") === "true";
    
     menuDesplegable.innerHTML = `
         <li ><a href="cart.html">Mi Carrito</a></li>
         <li ><a href="my-profile.html">Mi Perfil</a></li>
-         
-        <li> <div class="doble">
-           <li><<button id="modoDia">Modo Día</button> </li>
-          <button id="modoNoche">Modo Noche</button>
-          </div> </li>
+         <div class="modos">
+           <li><button id="modoDia">Modo Día</button> </li>
+           <li><button id="modoNoche">Modo Noche</button>  </li>
+          </div> 
     `;
+
     menuDesplegable.style.display = "none";
 nombreUsuarioButton.addEventListener("click", function (){
-
+    
     const rect = nombreUsuarioButton.getBoundingClientRect();
     const top = rect.bottom;
     const left = rect.left;
@@ -33,6 +36,8 @@ nombreUsuarioButton.addEventListener("click", function (){
     menuDesplegable.style.position = "fixed";
     menuDesplegable.style.top = `${top}px`;
     menuDesplegable.style.left = `${left}px`;
+
+   
   
     if (menuDesplegable.style.display === "none") {
         menuDesplegable.style.display = "block";
@@ -41,9 +46,32 @@ nombreUsuarioButton.addEventListener("click", function (){
         menuDesplegable.style.display = "block";
         
     }
+
+
+
+
+    
+    
 });
 document.body.appendChild(menuDesplegable);
-});
+
+
+
+/*MODOS*/
+
+document.getElementById("modoDia").addEventListener("click", function () {
+    menuDesplegable.classList.remove("modo-noche");
+    menuDesplegable.classList.add("modo-dia");
+    console.log("modo DIA");
+  });
+  
+  document.getElementById("modoNoche").addEventListener("click", function () {
+    menuDesplegable.classList.remove("modo-dia");
+    menuDesplegable.classList.add("modo-noche");
+    console.log("modo NOCHE")
+  }); 
+}); 
+
 
 
 
