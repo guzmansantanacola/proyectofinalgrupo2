47155e6d5906ef0c6b8da36bfb07ecc4644b45cf
guzmansantanacola <guzmansantanacola@gmail.com>
2023-10-11 14:18:55 -0300

    update

--- js/cart.js
+++ js/cart.js
@@ -28,7 +28,7 @@ function cartArticulos(data) {
  
         `;
     tabla.innerHTML = htmlContentToAppend;
-
+    
     // let inputPruebaExample = document.getElementById('50924input');
     // let partedelsubtotalExample = document.getElementsByClassName('cantidad');
     // console.log(partedelsubtotalExample[0]);
@@ -55,13 +55,12 @@ itemsLocalStorage.forEach((i, index) => {
     .then((data) => {
       const productoHTML = document.createElement("tr");
       productoHTML.id = data.id;
-
       const htmlContentToAppend = `
         <tr>
           <td><img class="imagencarrito" src="${data.images[0]}" style="width: 140px;"></td>
           <td><p>${data.name}</p></td>
           <td><p class="precioProducto">${data.currency} ${data.cost}</p></td>
-          <td><input type="number" value="${i.mount}" class="cantidad"></td>
+          <td><input min="1" type="number" value="${i.mount}" class="cantidad"></td>
           <td><p class="subTotal">Sub-Total: ${data.currency} <span class="subtotal-valor">${data.cost * i.mount}</span></p></td>
         </tr>`
 
