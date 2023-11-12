ef672b1d0a3ceeb2618ed66951a8d92c8d6378df
joniita <joniita.d@gmail.com>
2023-11-11 21:04:08 -0300

    borro comentarios innecesarios

--- js/productExample.js
+++ js/productExample.js
@@ -4,7 +4,3 @@ let productExample = {
   } 
 
 localStorage.setItem("cartListExample", JSON.stringify(productExample));
-
-//   if(JSON.parse(localStorage.getItem("productExample")) != null) {
-//     localStorage.setItem("productExample", JSON.stringify(productExample));
-//   } 
