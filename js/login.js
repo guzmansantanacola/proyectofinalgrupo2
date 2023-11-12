a81cbed2d9f37e3df42a99566f8dc254910cff4c
joniita <joniita.d@gmail.com>
2023-11-11 21:04:48 -0300

    borro comentarios innecesarios del login

--- js/login.js
+++ js/login.js
@@ -1,38 +1,4 @@
 
-// capturamos el boton y agregamos el escuchador de eventos cambiando el valor a true y redireccionando
-
-// document
-//   .getElementById("formlogin")
-//   .addEventListener("submit", function () {
-//     event.preventDefault();
-//     localStorage.setItem("sesionIniciada", "true");
-//     localStorage.setItem("exampleExist", true);
-//     localStorage.setItem("nombredeusuario", document.getElementById("guardarusuario").value);
-//     window.location.href = "index.html";
-//   }); 
-
-
-  // document
-  // .getElementById("googleImg")
-  // .addEventListener("click", function () {
-  //   localStorage.setItem("sesionIniciada", "true");
-  //   window.location.href = "index.html";
-  // });
-
-  // document
-  // .getElementById("githubImg")
-  // .addEventListener("click", function () {
-  //   localStorage.setItem("sesionIniciada", "true");
-  //   window.location.href = "index.html";
-  // });
-
-  // document
-  // .getElementById("facebookImg")
-  // .addEventListener("click", function () {
-  //   localStorage.setItem("sesionIniciada", "true");
-  //   window.location.href = "index.html";
-  // });
-
   /* VALIDACIÓN DEL LOGIN */
 
   function validationLogin() {
